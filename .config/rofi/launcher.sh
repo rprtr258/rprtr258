5ffd1a00ec204cdf1e24601bd62cc28ac4f431c6
rprtr258 <poletansky@gmail.com>
2022-03-06 19:34:35 +0300

    update rofi config

--- .config/rofi/launcher.sh
+++ .config/rofi/launcher.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rofi -no-lazy-grab -show combi -modi combi,window,drun,run,ssh -combi-modi window,drun,run,ssh -theme ./themes/launcher.rasi
+rofi -no-lazy-grab -show combi -theme ./themes/launcher.rasi
