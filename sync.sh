f51cf574906337fc35022572b890cb1bb2ac9bd6
rprtr258 <poletansky@gmail.com>
2022-03-07 05:46:01 +0300

    Update sync script

--- sync.sh
+++ sync.sh
@@ -2,8 +2,10 @@ pushd ~/GTD/reference/linux_config/
     # remove old
     rm -rf .config/*
     rm .{bashrc,xinitrc} crontab
+
     # prepare dir for nvim
     mkdir .config/nvim/
+
     # copy actual configs
     cp -r ~/.config/{rofi,polybar,i3}/ .config/
     cp ~/.config/nvim/init.vim .config/nvim/
