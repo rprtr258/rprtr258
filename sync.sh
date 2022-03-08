746c457813d36a52c7af1895cf1d96c6768672c5
rprtr258 <poletansky@gmail.com>
2022-03-08 16:04:28 +0300

    Update sync

--- sync.sh
+++ sync.sh
@@ -8,6 +8,7 @@ pushd ~/GTD/reference/linux_config/
 
     # copy actual configs
     cp -r ~/.config/{rofi,polybar,i3,alacritty}/ .config/
+    cp -r ~/.config/mimeapps.list .config/
     cp ~/.config/nvim/init.vim .config/nvim/
     cp ~/.{bashrc,xinitrc,bash_colors} .
     sudo cat /var/spool/cron/crontabs/rprtr258 | rg -v '^# |^$' > crontab
