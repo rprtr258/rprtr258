15a68be453994945c3d807505acc719bfdf2ba9c
rprtr258 <poletansky@gmail.com>
2022-03-08 22:28:19 +0300

    add stashing

--- upd.sh
+++ upd.sh
@@ -4,7 +4,8 @@ if [ "$(git status | wc -l)" != "4" ]; then
     cd /home/rprtr258/GTD/reference/linux_config
     git push
     cd ../
-    git add linux_config
-    git commit -m "Autoupdate linux config submodule"
+    git stash
+        git add linux_config
+        git commit -m "Autoupdate linux config submodule"
 fi
 
