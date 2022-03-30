15a9d0e6aafa7c2f91a83a1c29ac4a5371bb34f4
Poletansky Viktor <poletansky@gmail.com>
2022-03-30 21:34:20 +0300

    Remove bad words

--- .config/rofi/pastes/dump.sh
+++ .config/rofi/pastes/dump.sh
@@ -4,6 +4,6 @@ done
 for file in *.php; do
     cat $file | jq '.c' | python3 -c 'import codecs;print(" ".join(codecs.escape_decode(input())[0].decode().replace("&&", "")[1:-1].split()))';
 done > data2.txt
-rm *.php
-cat data*.txt > data
+cat data*.txt | rg -v 'пид(о|а)р|даун' > data
+rm *.php data2.txt
 
